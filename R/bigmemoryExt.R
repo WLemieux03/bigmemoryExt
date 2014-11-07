8ec4d7f7c0a1a38f6e8b578670b75e8b06fd1f86
Charles Determan <cdeterman@hotmail.com>
2014-11-07 08:40:32 -0600

    add cbindBM function for big.matrix

--- R/bigmemoryExt.R
+++ R/bigmemoryExt.R
@@ -1,5 +1,6 @@
 #' @useDynLib bigmemoryExt
 
+
 cleanupcols <- function(cols=NULL, nc=NULL, colnames=NULL) {
   if (is.null(cols)) cols <- 1:nc
   else {
@@ -94,4 +95,149 @@ transposeBM <- function(x, cols=NULL, rows=NULL,
     for (i in 1:length(cols)) y[,i] <- x[rows,cols[i]]
   
   return(y)
+}
+
+#' @title Subtract a "big.matrix" from an integer
+#' @description This was written to provide subtraction capabilities to big.matrix objects.  The \code{"daxpy"}
+#' function only works with equal size matrices.  To avoid making another large matrix, this a wrapper of C++ around the 
+#' big.matrix object to subtract each element from the provided value.  It likely could use further optimization.
+#' It also includes column and row selection in addition to optionally filebacking.
+#' @param x A \code{"big.matrix"}
+#' @param value A numeric value (e.g. "2.5", "1L")
+#' @param cols Possible subset of columns for the transpose; could be numeric, named, or logical
+#' @param rows Possible subset of rows for the transpose; could be numeric, named, or logical
+#' @param y Optional destinitation object (matrix or big.matrix); if not specified, a big.matrix will be created
+#' @param type preferably specified (e.g. "integer", "double", etc.)
+#' @param separated use separated column organization of the data instead of column-major organization; 
+#' use with caution if the number of columns is large.
+#' @param backingfile the root name for the file(s) for the cache of x.
+#' @param backingpath
+#' @param descriptorfile
+#' @param binarydescriptor
+#' @param shared
+#' @return a \code{"big.matrix"}
+#' @export
+subtractIntBM <- function(x, value, cols=NULL, rows=NULL, 
+                      y=NULL, type=NULL, separated=NULL,
+                      backingfile=NULL, backingpath=NULL,
+                      descriptorfile=NULL, binarydescriptor=FALSE,
+                      shared=TRUE)
+{
+  cols <- cleanupcols(cols, ncol(x), colnames(x))
+  rows <- cleanuprows(rows, nrow(x), rownames(x))
+  if (nrow(x) > 2^31-1)
+    stop(paste("Too many rows to copy at this point in time;",
+               "this may be fixed in the future."))
+  if (is.null(type)) type <- typeof(x)
+  if (is.big.matrix(x)) {
+    if (is.null(separated)) separated <- is.separated(x)
+  } else {
+    separated <- FALSE
+  }
+  if (is.null(y)) {
+    y <- big.matrix(nrow=length(rows), ncol=length(cols), type=type, init=NULL,
+                    dimnames=dimnames(x), separated=separated,
+                    backingfile=backingfile, backingpath=backingpath,
+                    descriptorfile=descriptorfile,
+                    binarydescriptor=binarydescriptor, shared)
+  }
+  if (is.big.matrix(x) && is.big.matrix(y))
+    .Call("CsubtIntBM", x@address, y@address, as.double(rows), as.double(cols), as.integer(value),
+          getOption("bigmemory.typecast.warning"))
+  else
+    for (i in 1:length(cols)) y[,i] <- x[rows,cols[i]]
+  
+  return(y)
+}
+
+
+#' @title cbind functionality for class "big.matrix"
+#' @description This is used to bind columns to big.matrix objects, with the new copy optionally filebacked
+#' @param x A \code{"big.matrix"}
+#' @param y Columns to append, accepts \code{"big.matrix"} and \code{"matrix"} in addition to 
+#' \code{"numeric"} and \code{"integer"} (e.g. "2.5", "1L")
+#' @param binding Binding location, "right" or "left"
+#' @param cols.x Possible subset of columns from the x object; could be numeric, named, or logical
+#' @param cols.y Possible subset of columns from the y object if a matrix type object otherwise ignored; 
+#' could be numeric, named, or logical
+#' @param z Optional destinitation object (matrix or big.matrix); if not specified, a big.matrix will be created
+#' @param type preferably specified (e.g. "integer", "double", etc.)
+#' @param separated use separated column organization of the data instead of column-major organization; 
+#' use with caution if the number of columns is large.
+#' @param backingfile the root name for the file(s) for the cache of x.
+#' @param backingpath
+#' @param descriptorfile
+#' @param binarydescriptor
+#' @param shared
+#' @details This is simply a wrapper for \code{"big.matrix"} objects whereby a new \code{"big.matrix"} is
+#' initialized and subsequently filled with the submitted arguments (optionally filtered).
+#' @return a \code{"big.matrix"}
+#' @export
+cbindBM <- function(x, y, binding="right",
+                    cols.x=NULL, cols.y=NULL,
+                    z=NULL, type=NULL, separated=NULL,
+                    backingfile=NULL, backingpath=NULL,
+                    descriptorfile=NULL, binarydescriptor=FALSE,
+                    shared=TRUE)
+{
+  
+  if(class(y) == "big.matrix" | class(y) == "matrix"){
+    if(nrow(x) != nrow(y)){
+      stop(paste("The number of rows are not equal between matrices."))
+    }
+  }
+  
+  if (nrow(x) > 2^31-1)
+    stop(paste("Too many rows to copy at this point in time;",
+               "this may be fixed in the future."))
+  if (is.null(type)) type <- typeof(x)
+  if (is.big.matrix(x)) {
+    if (is.null(separated)) separated <- is.separated(x)
+  } else {
+    separated <- FALSE
+  }
+  
+  cols1 <- cleanupcols(cols.x, ncol(x), colnames(x))
+  
+  if(class(y) == "numeric" | class(y) == "integer"){
+    cols2 <- 1
+  }else{
+    cols2 <- cleanupcols(cols.y, ncol(y), colnames(y))    
+  }
+
+  
+  if (is.null(z)) {
+    z <- big.matrix(nrow=nrow(x), ncol=length(cols1)+length(cols2), type=type, init=NULL,
+                    dimnames=dimnames(x), separated=separated,
+                    backingfile=backingfile, backingpath=backingpath,
+                    descriptorfile=descriptorfile,
+                    binarydescriptor=binarydescriptor, shared)
+  }
+  
+  if(binding=="right"){
+    z[,1:length(cols1)] = x[,c(cols1)]
+    
+    if(is.big.matrix(y) | is.matrix(y)){
+      z[,(length(cols1)+1):(length(cols1)+length(cols2))] = y[,c(cols2)]
+    }
+    else if(class(y) == "numeric" | class(y) == "integer"){
+      z[,(length(cols1)+1)] = y
+    }
+  }else{
+    if(binding == "left"){    
+      if(is.big.matrix(y) | is.matrix(y)){
+        z[,1:(length(cols2))] = y[,c(cols2)]
+      }
+      else if(class(y) == "numeric" | class(y) == "integer"){
+        z[,1] = y
+      }
+      
+      z[,(length(cols2)+1):(length(cols1)+length(cols2))] = x[,c(cols1)]
+    }else{
+      stop("binding variable not recognized, should be 'right' or 'left'")
+    }
+  }
+  
+  
+  return(z)
 }
