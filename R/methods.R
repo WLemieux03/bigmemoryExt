3a088526580f50f7a8aae1f202b23fcf958e2e87
Charles Determan <cdeterman@hotmail.com>
2014-12-02 09:24:13 -0600

    remove unneeded methods, added S3 transpose method

--- R/methods.R
+++ R/methods.R
@@ -3,69 +3,14 @@
 ### S4 Methods ###
 ##################
 
-#' @export
-setMethod("Arith", c(e1="big.matrix", e2="numeric"),
-          function(e1,e2)
-            {
-            op = .Generic[[1]]
-            switch(op,
-                   `^` = powBM(e1, e2),
-                   `*` = multiplyIntBM(e1, e2, type="double"),
-                    stop("Undefined operation")
-                   )
-          }
-          )
+
+
+##################
+### S3 Methods ###
+##################
 
 #' @export
-setMethod("Arith", c(e1="numeric", e2="big.matrix"),
-          function(e1,e2)
-          {
-            op = .Generic[[1]]
-            switch(op,
-                   `/` = divideIntBM(e2, e1, type="double"),
-                   `*` = multiplyIntBM(e2, e1, type="double"),
-                   stop("Undefined operation")
-            )
-          }
-)
-
-#' @export
-setMethod("Arith", c(e1="big.matrix", e2="big.matrix"),
-          function(e1,e2)
-          {
-            op = .Generic[[1]]
-            switch(op,
-                   `/` = divideMatrixBM(e1, e2, type="double"),
-                   `*` = multiplyMatrixBM(e1, e2, type="double"),
-                   stop("Undefined operation")
-            )
-          }
-)
-
-#' @export
-setMethod("Math", c(x="big.matrix"),
-          function(x)
-          {
-            op = .Generic[[1]]
-            switch(op,
-                   `sqrt` = powBM(x, 0.5),
-                   `exp` = expBM(x, type="double"),
-                   `log10` = log10BM(x, type="double"),
-                   #`log` = logBaseBM(x, type="double", base=base),
-                   `tanh` = tanhBM(x),
-                   `cosh` = coshBM(x),
-                   `sinh` = sinhBM(x),
-                   stop("Undefined operation")
-            )
-          }
-)
-
-#' @export
-setMethod("log", signature(x="big.matrix"),
-          function(x, base=exp(1))
-            {
-            logBaseBM(x, type="double", base=base)
-          }
-          )
-
-
+t.big.matrix <- function(x)
+{
+  return(transposeBM(x))
+}
